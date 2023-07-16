41631a6939e1c4329a4f3ee80a4ab521ebe6c818
Zhenbang You <zhenbang_you@apple.com>
2023-07-15 20:28:10 -0700

    ver2

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,17 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let receiver = TcpListener::bind("127.0.0.1:8080").await?;
+    let receiver = TcpListener::bind("localhost:8080").await?;
+    dbg!(receiver.local_addr().unwrap());
 
-    let mut sender = TcpStream::connect("127.0.0.1:8080").await?;
+    let mut sender = TcpStream::connect("[::1]:8080").await?;
     let sender_res = sender.write(b"12345".as_slice());
 
     let (mut stream, _addr) = receiver.accept().await?;
-    dbg!();
-    let mut res = String::new();
-    dbg!();
     sender_res.await?;
     dbg!(_addr);
     let mut buf: [u8; 5] = [0; 5];
-    // DOESN'T work:
-    // stream.read_to_string(&mut res).await?;
     stream.read(&mut buf).await?;
-    dbg!(from_utf8(&buf).unwrap(), res);
+    dbg!(from_utf8(&buf).unwrap());
     Ok(())
 }
