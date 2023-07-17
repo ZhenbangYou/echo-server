22eddb29d91974367e34d783b0db6d964cbd61bf
Zhenbang You <zhenbang_you@apple.com>
2023-07-16 17:20:59 -0700

    ver4

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
-use std::str::from_utf8;
+use futures::future::join_all;
+use std::thread;
+use std::time::Instant;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let receiver = TcpListener::bind("17.149.238.46:65535").await?;
-    dbg!(receiver.local_addr().unwrap());
+const SOCK_ADDR: &str = "[::1]:9009";
+const FILES: usize = 150;
+const CONTENT: [u8; 10] = [b'q'; 10];
 
-    let mut sender = TcpStream::connect("17.149.238.46:65535").await?;
-    dbg!();
-    let sender_res = sender.write(b"12345".as_slice());
-
-    let (mut stream, _addr) = receiver.accept().await?;
-    sender_res.await?;
-    dbg!(_addr);
-    let mut buf: [u8; 5] = [0; 5];
-    stream.read(&mut buf).await?;
-    dbg!(from_utf8(&buf).unwrap());
+async fn send_one(sock_addr: &str) -> io::Result<()> {
+    let mut s = TcpStream::connect(sock_addr).await?;
+    let mut in_buf = [0; CONTENT.len()];
+    s.write(&CONTENT).await?;
+    let _cnt = s.read(&mut in_buf).await?;
+    assert!(in_buf.eq(&CONTENT));
     Ok(())
+}
+
+fn send(sock_addr: &str) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(join_all((0..FILES).map(|_| send_one(sock_addr))));
+}
+
+async fn receive_one(mut tcp_stream: TcpStream) -> io::Result<()> {
+    let mut in_buf = [0; CONTENT.len()];
+    tcp_stream.read(&mut in_buf).await?;
+    tcp_stream.write(&in_buf).await?;
+    Ok(())
+}
+
+fn receive(sock_addr: &str) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let receiver = TcpListener::bind(sock_addr).await.unwrap();
+        let mut workers = vec![];
+        for _ in 0..FILES {
+            let (s, _) = receiver.accept().await.unwrap();
+            workers.push(receive_one(s));
+        }
+        join_all(workers).await
+    });
+}
+
+fn main() {
+    let start = Instant::now();
+
+    let t1 = thread::spawn(|| send(&SOCK_ADDR));
+    let t2 = thread::spawn(|| receive(&SOCK_ADDR));
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    let duration = start.elapsed();
+    println!("{:?}", duration);
 }
