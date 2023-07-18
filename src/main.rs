edbc8a48eb5be04e18b5f2ab67883f1190a302d6
Zhenbang You <zhenbang_you@apple.com>
2023-07-17 18:35:11 -0700

    ver6

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 const SOCK_ADDR: &str = "127.0.0.1:9009";
-const NUM_CONNECTIONS: usize = 100;
+const NUM_CONNECTIONS: usize = 10;
 const CONTENTS: [u8; 10] = [b'q'; 10];
 
 async fn send_one(sock_addr: &str, contents: &[u8]) -> io::Result<()> {
@@ -16,10 +16,9 @@ async fn send_one(sock_addr: &str, contents: &[u8]) -> io::Result<()> {
     Ok(())
 }
 
-fn send(sock_addr: &str, contents: &[u8]) {
-    let rt = tokio::runtime::Runtime::new().unwrap();
+fn send(rt: &tokio::runtime::Runtime) {
     rt.block_on(join_all(
-        (0..NUM_CONNECTIONS).map(|_| send_one(sock_addr, contents)),
+        (0..NUM_CONNECTIONS).map(|_| tokio::spawn(send_one(SOCK_ADDR, &CONTENTS))),
     ));
 }
 
@@ -30,26 +29,27 @@ async fn receive_one(mut tcp_stream: TcpStream) -> io::Result<()> {
     Ok(())
 }
 
-fn receive(sock_addr: &str) {
-    let rt = tokio::runtime::Runtime::new().unwrap();
+fn receive(rt: &tokio::runtime::Runtime) {
     rt.block_on(async {
-        let receiver = TcpListener::bind(sock_addr).await.unwrap();
-        let mut workers = vec![];
+        let receiver = TcpListener::bind(SOCK_ADDR).await.unwrap();
+        let mut join_handles = vec![];
         for _ in 0..NUM_CONNECTIONS {
             let (s, _) = receiver.accept().await.unwrap();
-            workers.push(receive_one(s));
+            join_handles.push(tokio::spawn(receive_one(s)));
         }
-        join_all(workers).await
+        join_all(join_handles).await
     });
 }
 
 fn main() {
     let start = Instant::now();
 
-    let t1 = thread::spawn(|| send(&SOCK_ADDR, &CONTENTS));
-    let t2 = thread::spawn(|| receive(&SOCK_ADDR));
-    t1.join().unwrap();
-    t2.join().unwrap();
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    thread::scope(|s| {
+        s.spawn(|| send(&rt));
+        s.spawn(|| receive(&rt));
+    });
 
     let duration = start.elapsed();
     println!("{:?}", duration);
